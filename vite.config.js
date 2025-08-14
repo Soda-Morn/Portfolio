08c095951f72cf658c817cfa5e4691f082bbd680
Soda-Morn <mornsoda23@gmail.com>
2025-08-14 11:03:35 +0700

    added the PWA

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'auto',
+      includeAssets: ['/icons/image.png'],
+      manifest: {
+        name: 'Soda Morn',
+        short_name: 'Soda Morn',
+        description: 'Soda Morn Portfolio',
+        theme_color: '#ffffff',
+        background_color: '#f0f0f0',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/soda2.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/soda1.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
